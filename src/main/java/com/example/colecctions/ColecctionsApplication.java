63c14afdcf81a34729abdcbe91b01356f6234c11
SONEKT <shulmanlevgen@gmail.com>
2023-07-22 20:29:31 +0500

    1

---
+++ src/main/java/com/example/colecctions/ColecctionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.colecctions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColecctionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColecctionsApplication.class, args);
+    }
+
+}
